8b192af6f4ce7bb22eca48637e851fd4c947d2d9
Mel <mel.vega15@gmail.com>
2019-08-26 14:06:24 -0500

    update file

--- bin/fastqc_samples.sh
+++ bin/fastqc_samples.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-#This script 
+#This script realize the quality analysis of sequencing with fastqc
 
 for i in GhB11_2_R1.fastq.gz GhB11_2_R2.fastq.gz GhY3-1_R1.fastq.gz GhY3-1_R2.fastq.gz GhOaxC3_R1.fastq.gz GhOaxC3_R2.fastq.gz;
 do fastqc $i -o ./
