570fadc0ba319b0d9f4b844698d5d4cb64dcf30e
Mel <mel.vega15@gmail.com>
2019-09-02 15:29:08 -0500

    update file

--- bin/data_cleaning.sh
+++ bin/data_cleaning.sh
@@ -1,12 +1,15 @@
 #!/bin/bash/
 
-# This script is for data cleaning using BBTools and Trim Galore.
-# Run from directory /bin/  and  the sequences they are in /data/R1 and /data/R2
+# This script is for data cleaning using BBTools.
+# Run this script from directory /bin/  and  the sequences they are in /data/R1 and /data/R2
 # Prerequisites: install BBTools 38.49
 
-# 1. Eliminate adapters
+# 1. Create output directories 
+mkdir ../data/seq_clean
 mkdir ../data/seq_clean/R1
 mkdir ../data/seq_clean/R2
+
+# 2. Eliminate adapters
 for i in *.fastq.gz;
 do bbduk.sh -Xmx2g threads=1 in1=../data/R1/$i in2=../data/R2/$i out1=../data/seq_clean/R1/$i out2=../data/R2/$i ref=/resources/adapters.fa tpe tbo
 
@@ -15,3 +18,4 @@ do bbduk.sh -Xmx2g threads=1 in1=../data/R1/$i in2=../data/R2/$i out1=../data/se
 # 3. Eliminate sequences with low quality 
 
 # 4. Eliminate overrepresented sequences
+trim_galore --fastqc --paired --retain_unpaired -a2 GGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGG -o ./clean ./GhB11_2_R1_filter3.fastq.gz ./GhB11_2_R2_filter3.fastq.gz
--- bin/fastqc_samples.sh
+++ bin/fastqc_samples.sh
@@ -2,7 +2,7 @@
 
 #This script do the quality analysis of sequencing with fastqc
 
-for i in ../data/*.fastq.gz;
+for i in ../data/raw/*.fastq.gz;
 do fastqc $i -o ./
 done
 
