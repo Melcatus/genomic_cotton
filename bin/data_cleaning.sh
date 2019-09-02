244558be4a5568fccba762faef9f9ce1bd58b525
Melania Vega <36024079+Melcatus@users.noreply.github.com>
2019-09-02 15:42:17 -0500

    Update data_cleaning.sh

--- bin/data_cleaning.sh
+++ bin/data_cleaning.sh
@@ -13,9 +13,3 @@ mkdir ../data/seq_clean/R2
 for i in *.fastq.gz;
 do bbduk.sh -Xmx2g threads=1 in1=../data/R1/$i in2=../data/R2/$i out1=../data/seq_clean/R1/$i out2=../data/R2/$i ref=/resources/adapters.fa tpe tbo
 
-# 2. Eliminate sequences of phago phix
-
-# 3. Eliminate sequences with low quality 
-
-# 4. Eliminate overrepresented sequences
-trim_galore --fastqc --paired --retain_unpaired -a2 GGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGG -o ./clean ./GhB11_2_R1_filter3.fastq.gz ./GhB11_2_R2_filter3.fastq.gz
