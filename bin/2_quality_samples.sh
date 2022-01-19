d290a4de1297995b321e56cf4f6ae6374a812e5e
Melcatus <mel.vega15@gmail.com>
2022-01-18 23:47:47 -0600

    correction of path

--- bin/2_quality_samples.sh
+++ bin/2_quality_samples.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
 # This script do the quality analysis of sequencing with fastqc
-# Run this script from directory /bin/  and  the sequences they are in /data/raw/resequenced/  and in /data/raw/published/
+# Run this script from directory /bin/  and  the sequences they are in /data/raw/new_genomes/  and in /data/raw/published/
 # Prerequisites: install FastQC v.0.11.7 
 
 # Create output directory
-mkdir ../data/raw/resequenced/fastqcs
+mkdir ../data/raw/new_genomes/fastqcs
 
 # Do analysis sequences quality 
-for i in ../data/raw/resequenced/*.fastq.gz;
-do fastqc $i -o ../data/raw/resequenced/fastqcs
+for i in ../data/raw/new_genomes/*.fastq.gz;
+do fastqc $i -o ../data/raw/new_genomes/fastqcs
 done
 
 # Create output directory 
