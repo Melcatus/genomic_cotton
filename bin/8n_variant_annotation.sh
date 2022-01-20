92144c669ece9d775c2b2eb82e1ec295f86887f2
Melcatus <mel.vega15@gmail.com>
2022-01-19 21:14:05 -0600

    added fixed steps

--- bin/8n_variant_annotation.sh
+++ bin/8n_variant_annotation.sh
@@ -16,9 +16,26 @@ for R in `ls ../data/raw/published/clean/next/ | grep -oE "\w*_" | uniq`;
 do java -jar -Xms50000M -Xmx50000M ../NGSEPcore_4.0.3.jar VCFAnnotate -i ../data/raw/published/clean/next/${R}3.vcf -t ../data/raw/reference/TM-1_V2.1.gene2.gff3 -r ../data/raw/reference/TM-1_V2.1.fa -o ../data/raw/published/clean/next/${R}annotate.vcf;
 done
 
-######## Merge vcf by population #######
-java -jar -Xms50000M -Xmx50000M ../NGSEPcore_3.3.0.jar MergeVCF ../data/raw/reference/TM-1_V2.1.fa.fai ../data/raw/new_genomes/trim/next/A01_3.vcf ../data/raw/new_genomes/trim/next/A02_3.vcf ../data/raw/published/clean/next/A04_3.vcf ../data/raw/published/clean/next/A05_3.vcf 1> ../data/raw/new_genomes/trim/next/wild.vcf 2> ../data/raw/new_genomes/trim/next/1populationwild.log
+#### separate files of Indels, STR and SNP
+#####make directory to indel files
+mkdir indels
+#### eliminate snps and STR and select Indels
+for R in `ls ./ | grep -oE "\w*_" | uniq`;
+do grep "INDEL" ./${R}annotate.vcf | grep -v "Scaffold" | grep -v "scaffold" > ./indels/${R}indel.vcf;
+done
 
-java -jar -Xms50000M -Xmx50000M ../NGSEPcore_3.3.0.jar MergeVCF ../data/raw/new_genomes/trim/next/C01_3.vcf ../data/raw/new_genomes/trim/next/C02_3.vcf ../data/raw/new_genomes/trim/next/C03_3.vcf ../data/raw/new_genomes/trim/next/C04_3.vcf ../data/raw/new_genomes/trim/next/C05_3.vcf 1> ../data/raw/new_genomes/trim/next/landraces.vcf 2> ../data/raw/new_genomes/trim/next/1populationland.log
+#####make directory to indel files
+mkdir str
 
-java -jar -Xms50000M -Xmx50000M ../NGSEPcore_3.3.0.jar MergeVCF ../data/raw/published/clean/next/E01_3.vcf ../data/raw/published/clean/next/E02_3.vcf ../data/raw/published/clean/next/E03_3.vcf ../data/raw/published/clean/next/E04_3.vcf ../data/raw/published/clean/next/E05_3.vcf ../data/raw/published/clean/next/F01_3.vcf 1> ../data/raw/new_genomes/trim/next/commercial.vcf 2> ../data/raw/new_genomes/trim/next/1populationcomm.log
+#### eliminate snps and indels and select STR
+for R in `ls ./ | grep -oE "\w*_" | uniq`;
+do grep "STR" ./${R}annotate.vcf | grep -v "Scaffold" | grep -v "scaffold" > ./str/${R}str.vcf;
+done
+
+#####make directory to indel files
+mkdir snp
+for R in `ls ./ | grep -oE "\w*_" | uniq`;
+do grep -v "TYPE=INDEL" ./${R}annotate.vcf | grep -v "TYPE=STR" | grep -v "Scaffold" | grep -v "scaffold"  > ./snp/${R}fil4.vcf;
+done
+
+
